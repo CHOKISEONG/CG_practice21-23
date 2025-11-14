46d4c12798826421a892ccd2bc6bc3e0fc1f6337
Cho Kisung <kisung128@gmail.com>
2025-11-14 21:18:59 +0900

    tmp

--- CG_practice21-23/Camera.h
+++ CG_practice21-23/Camera.h
@@ -15,23 +15,15 @@ private:
 	float height = 1080.0f;	// 높이
 	float zNear = 0.1f;		// 가까운 클리핑 평면까지의 거리
 	float zFar = 100.0f;	// 먼	 클리핑 평면까지의 거리
-
-	bool isRotating = false;
-	bool isRotateSelf = false;
-	float rotateSpeed = 0.5f;
 public:
 	void settingCamera(GLuint shaderProgram);
 
 	const glm::vec3 getPos() const { return pos; }
 
-	void update();
 	void changeWidthHeight(float w, float h) { width = w, height = h; }
 	void move(float x, float y, float z = 0.0f) { pos.x += x; pos.y += y; pos.z += z; }
 
 	// rotateX가 true면 x축회전, false면 y축회전으로 함
 	void rotate(float theta, glm::vec3 axis = glm::vec3(0.0f,1.0f,0.0f));
 	void rotateFromView(float theta, glm::vec3 axis = glm::vec3(0.0f, 1.0f, 0.0f));
-	void rotateStart(float speed) { isRotating = true; isRotateSelf = false; rotateSpeed = speed; }
-	void rotateStartY(float speed) { isRotating = true; isRotateSelf = true; rotateSpeed = speed; }
-	void rotateStop() { isRotating = false; rotateSpeed = 0.0f; }
 };
